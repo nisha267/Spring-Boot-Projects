aaf15d500e8022b1fd8a22d282ed9af0b4613cea
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2023-03-22 12:10:42 +0530

    Delete MedicalManagementSystems directory

--- MedicalManagementSystems/src/main/java/com/medical/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medical.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medical.entity.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
-
-}
--- MedicalManagementSystems/src/main/java/com/medical/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medical.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.medical.entity.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-
-}
